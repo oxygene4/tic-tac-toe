9461319add0a289b93f2906b6813972e1d6aed89
Eugen Hovorun <yevhen.hovorun@extrawest.com>
2019-12-26 11:39:12 +0200

    add gameOver checking

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,9 +16,11 @@ export class HomeComponent implements OnInit {
   userId: string;
   isPlaying = false;
   isLoading = false;
+  isGameOver = false;
   X_MARK = 'X';
   O_MARK = 'O';
   gameModel: string[] = ['', '', '', '', '', '', '', '', ''];
+  winnerLine: string;
   userMark = this.X_MARK;
   currentPlayerMark = this.X_MARK;
 
@@ -54,10 +56,30 @@ export class HomeComponent implements OnInit {
     return this.userMark === this.X_MARK ? this.O_MARK : this.X_MARK;
   }
 
+  get gameModelObject() {
+    return {
+      row1: [this.gameModel[0], this.gameModel[1], this.gameModel[2]],
+      row2: [this.gameModel[3], this.gameModel[4], this.gameModel[5]],
+      row3: [this.gameModel[6], this.gameModel[7], this.gameModel[8]],
+      col1: [this.gameModel[0], this.gameModel[3], this.gameModel[6]],
+      col2: [this.gameModel[1], this.gameModel[4], this.gameModel[7]],
+      col3: [this.gameModel[2], this.gameModel[5], this.gameModel[8]],
+      dia1: [this.gameModel[0], this.gameModel[4], this.gameModel[8]],
+      dia2: [this.gameModel[2], this.gameModel[4], this.gameModel[6]]
+    };
+  }
+
+  get winnerCells() {
+    // todo fix winnerCells definition
+    return this.gameModelObject[this.winnerLine] || [];
+  }
+
   choosePlayer(chosenMark) {
     this.gameModel = ['', '', '', '', '', '', '', '', ''];
     this.currentPlayerMark = this.X_MARK;
     this.userMark = chosenMark;
+    this.isGameOver = false;
+    this.winnerLine = '';
 
     if (chosenMark !== this.X_MARK) {
       this.makeStep();
@@ -67,13 +89,12 @@ export class HomeComponent implements OnInit {
   }
 
   userStepHandler(cellIndex) {
-    if (this.gameModel[cellIndex] || this.isLoading) {
+    if (this.gameModel[cellIndex] || this.isLoading || this.isGameOver) {
       return;
     }
     this.isLoading = true;
     this.gameModel[cellIndex] = this.userMark;
     this.checkWinner();
-    this.toggleCurrentPlayer();
     this.makeStep();
   }
 
@@ -82,7 +103,6 @@ export class HomeComponent implements OnInit {
       .then((data: any) => {
         this.gameModel[data.recommendation] = this.opponentMark;
         this.checkWinner();
-        this.toggleCurrentPlayer();
         this.isLoading = false;
       });
   }
@@ -92,23 +112,19 @@ export class HomeComponent implements OnInit {
   }
 
   checkWinner() {
-    const gameModel = this.gameModel;
-    const matrix = {
-      row1: [gameModel[0], gameModel[1], gameModel[2]],
-      row2: [gameModel[3], gameModel[4], gameModel[5]],
-      row3: [gameModel[6], gameModel[7], gameModel[8]],
-      col1: [gameModel[0], gameModel[3], gameModel[6]],
-      col2: [gameModel[1], gameModel[4], gameModel[7]],
-      col3: [gameModel[2], gameModel[5], gameModel[8]],
-      dia1: [gameModel[0], gameModel[4], gameModel[8]],
-      dia2: [gameModel[2], gameModel[4], gameModel[6]]
-    };
+    const finished = this.gameModel.every(Boolean);
+    const filledLines = Object.entries(this.gameModelObject).filter(([_, cells]) => cells.every(Boolean));
 
-    const result = Object.entries(matrix).find(([_, cells]) => {
-      return cells.every(Boolean) && cells.every( (mark, index, self) => mark === self[0] );
+    const hasWinnerLine = filledLines.find(([_, cells]) => {
+      return cells.every((mark, index, self) => mark === self[0]);
     });
 
-    console.log(result);
+    this.winnerLine = hasWinnerLine ? hasWinnerLine[0] : '';
+    this.isGameOver = finished || !!this.winnerLine;
+
+    if (!this.isGameOver) {
+      this.toggleCurrentPlayer();
+    }
   }
 
   finishGame() {
