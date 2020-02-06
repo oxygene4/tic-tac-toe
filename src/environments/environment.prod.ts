a291edb1673a1a1ac72d5a0a805534f0b90e42c7
Eugen Hovorun <yevhen.hovorun@extrawest.com>
2020-02-06 17:58:26 +0200

    upate env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCmw1OJanX27n9rDxR4IaA-SQqv0hYpMiQ',
+    authDomain: 'govorun-project.firebaseapp.com',
+    databaseURL: 'https://govorun-project.firebaseio.com',
+    projectId: 'govorun-project',
+    storageBucket: 'govorun-project.appspot.com',
+    messagingSenderId: '709367050757',
+    appId: '1:709367050757:web:1c8a553cc858e341fbe26c'
+  },
+  rapidApiTicTacToe: {
+    key: 'PDirDBiguWmshbliGep4aPqjjBffp1bwhSSjsnPZeNff3jdd91',
+    host: 'stujo-tic-tac-toe-stujo-v1.p.rapidapi.com',
+    base: 'https://stujo-tic-tac-toe-stujo-v1.p.rapidapi.com/'
+  }
 };
